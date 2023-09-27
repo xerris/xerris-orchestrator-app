4d006bea2b15fd699851ffaec5652c09bbf12d82
R-Ram95 <rohinesh@ualberta.ca>
2023-09-27 17:02:29 -0600

    deployment: added import-map- overrides

--- src/xerris-root-config.ts
+++ src/xerris-root-config.ts
@@ -1,18 +1,9 @@
 /* eslint-disable no-console */
 import { LifeCycles, registerApplication, start } from "single-spa";
 
-const authModule = "@xerris/auth-app";
 const calendarModule = "@xerris/calendar-app";
-const sideBarModule = "@xerris/sidebar-app";
-const homeModule = "@xerris/home-app";
 
 export function runSpas() {
-  registerApplication({
-    name: "@xerris/auth-app",
-    app: () => System.import<LifeCycles>(authModule),
-    activeWhen: [(location) => location.pathname === "/"],
-  });
-
   registerApplication({
     name: "@xerris/calendar-app",
     app: () =>
@@ -22,24 +13,6 @@ export function runSpas() {
       ),
     activeWhen: [(location) => location.pathname === "/calendar"],
   });
-
-  registerApplication({
-    name: "@xerris/sidebar-app",
-    app: () => System.import<LifeCycles>(sideBarModule),
-    activeWhen: [(location) => location.pathname !== "/"],
-  });
-
-  registerApplication({
-    name: "@xerris/home-app",
-    app: () => System.import<LifeCycles>(homeModule),
-    activeWhen: ["/home"],
-  });
-
-  // registerApplication({
-  //   name: "@xerris/pricing-calculator",
-  //   app: () => System.import("@xerris/pricing-calculator"),
-  //   activeWhen: ["/pricing-calculator"],
-  // });
 
   start();
 }
